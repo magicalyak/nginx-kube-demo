99ec4d5e17b32b0fb41cf60620aeba12beb95f1b
magicalyak <tgamull@redhat.com>
2019-03-31 11:52:25 -0400

    typo in uninstall

--- scripts/uninstall.sh
+++ scripts/uninstall.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 cd ../kubespray
 vagrant destroy -f
-rm inventory/sample/vagrant_ansible_invetory
+rm inventory/sample/vagrant_ansible_inventory
 rm -rf inventory/sample/artifacts
 rm ~/.kube/config
