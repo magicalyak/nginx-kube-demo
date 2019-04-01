cc5d39cb37f36a13f7fb34ff4d071ec56f1074ee
magicalyak <tgamull@redhat.com>
2019-04-01 11:21:02 -0400

    added inventory.sh commands

--- scripts/install.sh
+++ scripts/install.sh
@@ -2,3 +2,6 @@
 cd ../kubespray
 ansible-playbook ../nginx-kube-demo/deploy-dashboard.yml -i inventory/sample/vagrant_ansible_inventory --become --become-user=root
 ansible-playbook ../nginx-kube-demo/deploy-nginx.yml -i inventory/sample/vagrant_ansible_inventory --become --become-user=root
+cp inventory/sample/artifacts/admin.conf ~/.kube/config
+kubectl -n kube-system describe secret $(kubectl -n kube-system get secret | grep admin-user | awk '{print $1}')
+kubectl cluster-info
